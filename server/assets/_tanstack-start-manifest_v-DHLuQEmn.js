26994b0fac87b197371837df5697e6e62162f5bd
Leticia <0001159429@senaimgaluno.com.br>
2026-04-16 20:32:53 -0300

    Updates

--- server/assets/_tanstack-start-manifest_v-DHLuQEmn.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/Letícia/Documents/GP/diagnostico-maquinas-projeto-completo/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/Diagnostico-de-Maquinas/assets/index-Dsi7cJQ0.js"] }, "/": { filePath: "C:/Users/Letícia/Documents/GP/diagnostico-maquinas-projeto-completo/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/Diagnostico-de-Maquinas/assets/index-cL0SIKHB.js"] } }, clientEntry: "/Diagnostico-de-Maquinas/assets/index-Dsi7cJQ0.js" });
-export {
-  tsrStartManifest
-};
